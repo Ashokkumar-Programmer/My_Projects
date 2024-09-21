b3ca08aaf4abe32f9fb1688eebe2177fcc547a6c
ashokkumar-webhosting <ashokkumar.webhosting@gmail.com>
2024-09-21 09:09:54 +0530

    Delete E-Commerce directory

--- E-Commerce/src/main/java/backend/dao/UserRespository.java
+++
@@ -1,9 +0,0 @@
-package backend.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import backend.entity.UserData;
-
-
-public interface UserRespository extends JpaRepository<UserData, String> {
-	UserData findByUsername(String username);
-}
